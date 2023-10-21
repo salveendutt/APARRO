0c176c690ddc4eb71dfbe795dbaf05edb045189b
Salveen <mrsalveen@gmail.com>
2023-10-21 21:25:23 +0200

    Adding task description

--- whisper.py
+++ whisper.py
@@ -3,7 +3,7 @@ import io
 import os
 import speech_recognition as sr
 import torch
-
+COMPUTE_TYPE = "float16"
 from datetime import datetime, timedelta
 from queue import Queue
 from tempfile import NamedTemporaryFile
@@ -11,7 +11,7 @@ from time import sleep
 
 from faster_whisper import WhisperModel
 
-audio_model = WhisperModel("medium.en", device="cuda", compute_type="float16")
+audio_model = WhisperModel("medium.en", device="cuda", compute_type=COMPUTE_TYPE)
 
 # Create a class for whisper, where you can initialize this class in another file
 # and you will be able to do text = my_model.start_recording(smth) and text will
@@ -26,3 +26,31 @@ audio_model = WhisperModel("medium.en", device="cuda", compute_type="float16")
 # mechanism) 
 
 # Hint, try to use OOD classes and a simplest factory method (or anything else)
+
+# class mevinWhiser:
+#   predicted_text = ""
+# and predicted_text will be changed to the current recording transcribed after 
+# calling the stop_recording method
+# 
+# def stop_recording:
+# pred_text = ....
+# self.predicted_text = pred_text
+# def GetPredictedText:
+# return self.predicted_text
+
+## SOLID PRINCIPLE 
+# Every single method should have only one responsobility
+##############################################################################################
+# USER PERSPECTIVE
+# import mevinWhisper
+
+# def main():
+#     whisp_instance = mevinWhiper.Initialize(model_name="medium.en", device_type="cuda")
+    
+#     # This method will be listening to the mic, until 
+#     whisp_instance.start_recording(some_parameters...)
+    
+#     # will save the text in a field inside of the instance
+#     whisp_instance.stop_recording()
+    
+#     text = whisper_instance.GetPredictedText()
