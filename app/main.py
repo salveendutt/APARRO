13f0b9ebd26d1a32cf0eb2a295f25f4ab3dc6a5b
Salveen <mrsalveen@gmail.com>
2023-10-30 17:44:03 +0100

    Making main.py easier to demo

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def print_order(ordered, unavailable):
     print(f"Your order is: {ordered_str}")
     print(f"Unfortunately we don't have: {unavailable_str}\n")
     
-def mainB():
+def json_demo():
     backup_order = """
     {
         "dish": "MacCrispy",
@@ -41,7 +41,7 @@ def mainB():
     ordered, unavailable = waiter.process_order(dict_order)
     print_order(ordered, unavailable)
 
-def mainC():
+def whisper_demo():
     whisper_instance = tr(model_name="medium.en", device_type="cuda")
     print("Press 'O' to start recording...\n")
     keyboard.wait("o")
@@ -53,17 +53,18 @@ def mainC():
     text = whisper_instance.get_predicted_text()
     print("Predicted Text:", text)
 
-def main():
+def waiter_demo():
     order_str = """
     I would like to order McCrispy without pickle and 5 large french fries. Change 2 of french 
-    fries to 2 Big Mac and add a Coke. Also add one Chicken Biriani.
+    fries to 2 Big Mac and add a Cok`e. Also add one Chicken Biriani.
     """.strip()
     waiter = wt.Waiter(order_str)
     ordered, unavailable = waiter.take_order()
     print_order(ordered, unavailable)
 
 
+def main():
+    json_demo()
+    
 if __name__ == "__main__":
-    # main()
-    mainB()
-    # mainC()
+    main()
