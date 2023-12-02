c6a00de963e6b0a4ad136529e90361f360c3c74e
Gogetanks <01155543@pw.edu.pl>
2023-12-02 23:52:19 +0100

    Added Unit Tests for Transcriber: Part 1

--- tests/test_transcriber.py
+++ tests/test_transcriber.py
@@ -3,35 +3,38 @@ import io
 import os
 import sys
 import soundfile as sf
-# TODO Fix the path. Right now it gives warning, but something has to be done in order
-# for it to not show warning
+
 app_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app'))
-sys.path.append(app_dir)
+if app_dir not in sys.path:
+    sys.path.append(app_dir)
+
 import transcriber as tr  # Import the Transcriber class from your implementation
 
 class TestTranscriber(unittest.TestCase):
     def setUp(self):
         self.transcriber = tr.Transcriber(model_name="medium.en", device_type="cuda")
 
-    # TODO need to load from wav/flac file bytes and then using these bytes make a test
-    # from https://github.com/openai/whisper/tree/main/tests 
-    # def test_transcribe_audio(self):
-        # audio_path = os.path.join(os.path.dirname(__file__), r"testdata\jfk.wav")
-        # audio_data, _ = sf.read(audio_path)
-        # audio_buffer = io.BytesIO(audio_data)
-        # transcription = self.transcriber.transcribe_audio(audio_buffer)
-        # self.assertIsInstance(transcription, str)
+    def test_transcribe_audio(self):
+        audio_path = os.path.join(os.path.dirname(__file__), "testdata", "jfk.wav")
+        with open(audio_path, 'rb') as f:
+            audio_data = f.read()
+        audio_buffer = io.BytesIO(audio_data)
+        transcription = self.transcriber._transcribe_audio(audio_buffer)  # Use private method
+        self.assertIsInstance(transcription, str)
+        self.assertNotEqual(transcription, "")
 
     def test_start_recording_and_stop_recording(self):
-        self.transcriber.start_recording()
-        self.assertTrue(self.transcriber.recording)
-        self.transcriber.stop_recording()
-        self.assertFalse(self.transcriber.recording)
+        self.transcriber._start_recording()  # Use private method
+        self.assertTrue(self.transcriber._is_recording)  # Use private attribute
+        self.transcriber._stop_recording()  # Use private method
+        self.assertFalse(self.transcriber._is_recording)  # Use private attribute
 
     def test_get_predicted_text(self):
-        self.transcriber.transcription_done.set()  # Simulate transcription completion
-        predicted_text = self.transcriber.get_predicted_text()
+        self.transcriber._transcription_done.set()  # Use private attribute
+        predicted_text = self.transcriber._get_predicted_text()  # Use private method
         self.assertIsInstance(predicted_text, str)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
