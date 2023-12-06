3a141aef1d05e956197c18367cfad540610735df
Gogetanks <01155543@pw.edu.pl>
2023-12-06 17:08:57 +0100

    be

--- tests/test_transcriber.py
+++ tests/test_transcriber.py
@@ -2,7 +2,9 @@ import unittest
 import io
 import os
 import sys
+import numpy as np
 import soundfile as sf
+from unittest.mock import MagicMock, patch
 
 app_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app'))
 if app_dir not in sys.path:
@@ -11,8 +13,47 @@ if app_dir not in sys.path:
 import transcriber as tr  # Import the Transcriber class from your implementation
 
 class TestTranscriber(unittest.TestCase):
+
     def setUp(self):
         self.transcriber = tr.Transcriber(model_name="medium.en", device_type="cuda")
+
+    # def infinite_audio_stream():
+    # # Provide 10 chunks of mock data, then infinite empty data
+    #     for _ in range(10):
+    #         yield b'\x00\x01' * 256
+    #     while True:
+    #         yield b''
+
+    # @patch('pyaudio.PyAudio', autospec=True)
+    # def test_capture_audio(self, mock_pyaudio):
+    #     # Setup the mock
+    #     mock_audio_instance = mock_pyaudio.return_value
+    #     mock_stream = mock_audio_instance.open.return_value
+    #     mock_stream.read.side_effect = self.infinite_audio_stream()  # Use the infinite generator
+
+    #     # Start the test
+    #     self.transcriber._is_recording = True
+    #     self.transcriber._capture_audio()
+
+    #     # Assertions
+    #     mock_audio_instance.open.assert_called_once()
+    #     mock_stream.read.assert_called()
+    #     mock_stream.stop_stream.assert_called()
+    #     mock_stream.close.assert_called()
+    #     mock_audio_instance.terminate.assert_called()
+
+    # @patch.object(tr.Transcriber, '_transcribe_audio', return_value="Mocked transcription")
+    # def test_record_audio(self, mock_transcribe_audio):
+    #     # Setting up the state
+    #     self.transcriber._is_recording = True
+    #     self.transcriber._frames = [np.array([1, 2, 3])]  # Example frame data
+
+    #     # Call the method under test
+    #     self.transcriber._record_audio()
+
+    #     # Assert if _transcribe_audio was called
+    #     mock_transcribe_audio.assert_called()
+
 
     def test_transcribe_audio(self):
         audio_path = os.path.join(os.path.dirname(__file__), "testdata", "jfk.wav")
@@ -33,6 +74,19 @@ class TestTranscriber(unittest.TestCase):
         self.transcriber._transcription_done.set()  # Use private attribute
         predicted_text = self.transcriber._get_predicted_text()  # Use private method
         self.assertIsInstance(predicted_text, str)
+
+    def test_pause_and_resume_recording(self):
+        self.transcriber._start_recording()
+        self.assertTrue(self.transcriber._is_recording)
+        self.assertFalse(self.transcriber._is_paused)
+
+        self.transcriber._pause_recording()
+        self.assertFalse(self.transcriber._is_recording)
+        self.assertTrue(self.transcriber._is_paused)
+
+        self.transcriber._resume_recording()
+        self.assertTrue(self.transcriber._is_recording)
+        self.assertFalse(self.transcriber._is_paused)
 
     
 
