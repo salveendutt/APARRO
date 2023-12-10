37eb536288f8c7083b16e23a9b7aad11c6868181
Mevin Biju <88531280+Gogetanks@users.noreply.github.com>
2023-12-10 18:00:39 +0100

    implemented pause/resume features (#9)
    * implemented pause/resume features
    
    * Adding unit tests to pylint.yml
    
    * code analysis part 1
    
    * code analysis part 2
    
    * code analysis part 3
    
    * Adding fixes + unit tests to waiter.py
    
    * pylint fix
    
    * small fixes pylint
    
    * Adding comments in model/notebooks
    
    * Moving unnecessary files to tools/
    
    * Fixing pylint, adding docstrings
    
    * Removing pylint from tests folder
    
    As it is unnecessary, other official repositories do not use it
    
    * Added Unit Tests for Transcriber: Part 1
    
    * adding evaluation
    
    * moving evaluation
    
    * be
    
    * small fix in test_waiter.py
    
    * unit tests transcriber part 2
    
    * Final version with proper unit testing
    
    * 64 Trans
    
    * 64 Trans
    
    * Adding evaluation dataset
    
    * Fixing transcriber + changes in dataset for evaluation
    
    * Fixing evaluation, dataset
    
    ---------
    
    Co-authored-by: Salveen <77534378+mrsalveen@users.noreply.github.com>
    Co-authored-by: Salveen <mrsalveen@gmail.com>

--- app/transcriber.py
+++ app/transcriber.py
@@ -4,10 +4,12 @@ This module contains the Transcriber class for transcribing audio input using th
 
 import io
 import threading
+import time
 import numpy as np
 import pyaudio
 import soundfile as sf
 from faster_whisper import WhisperModel
+import keyboard
 
 COMPUTE_TYPE = "float16"
 # Whisper is able to handle only up to 30 seconds, so we need to cut the string
@@ -34,6 +36,7 @@ class Transcriber:
             raise RuntimeError("Error initializing WhisperModel") from e
 
         self._is_recording = False
+        self._is_paused = False
         self._predicted_text = ""
         self._frames = []
         self._transcription_done = threading.Event()
@@ -43,17 +46,26 @@ class Transcriber:
         Captures audio from the microphone and stores it in frames.
         """
         audio = pyaudio.PyAudio()
-        stream = audio.open(format=pyaudio.paInt16, channels=1,
-                            rate=16000, input=True, frames_per_buffer=1024)
+        stream = None
         try:
-            while self._is_recording:
-                data = stream.read(256)
-                self._frames.append(np.frombuffer(data, dtype=np.int16))
+            while self._is_recording or self._is_paused:
+                if self._is_recording:
+                    if stream is None:
+                        stream = audio.open(format=pyaudio.paInt16, channels=1,
+                                            rate=16000, input=True, frames_per_buffer=1024)
+
+                    data = stream.read(256, exception_on_overflow=False)
+                    self._frames.append(np.frombuffer(data, dtype=np.int16))
+                elif stream is not None:
+                    stream.stop_stream()
+                    stream.close()
+                    stream = None
         except IOError as e:
             raise RuntimeError("Error capturing audio") from e
         finally:
-            stream.stop_stream()
-            stream.close()
+            if stream is not None:
+                stream.stop_stream()
+                stream.close()
             audio.terminate()
 
     def _record_audio(self):
@@ -91,6 +103,21 @@ class Transcriber:
         Stops the audio recording process.
         """
         self._is_recording = False
+        self._is_paused = False
+
+    def _pause_recording(self):
+        """
+        Pauses the audio recording process.
+        """
+        self._is_paused = True
+        self._is_recording = False
+
+    def _resume_recording(self):
+        """
+        Resumes the audio recording process.
+        """
+        self._is_paused = False
+        self._is_recording = True
 
     def _get_predicted_text(self) -> str:
         """
@@ -120,7 +147,7 @@ class Transcriber:
         except Exception as e:
             raise RuntimeError("Error transcribing audio") from e
 
-    def transcribe(self) -> str:
+    def transcribe(self):
         """
         Perform audio recording and transcription.
 
@@ -133,9 +160,31 @@ class Transcriber:
         try:
             input("Press Enter to start recording...")
             self._start_recording()
-            input("Recording... Press Enter again to stop recording.")
-            self._stop_recording()
-            print("Recording Complete. Transcribing...\n")
+            print("Recording... (Press 'p' to pause, 'r' to resume, 's' to stop)")
+            last_press_time = 0
+            debounce_int = 0.5  # 500 milliseconds
+
+            while True:
+                current_time = time.time()
+
+                if keyboard.is_pressed('p') and current_time - last_press_time > debounce_int:
+                    self._pause_recording()
+                    print("Recording paused. Press 'r' to resume or 's' to stop.")
+                    last_press_time = current_time
+
+                elif keyboard.is_pressed('r') and current_time - last_press_time > debounce_int:
+                    self._resume_recording()
+                    print("Recording resumed. Press 'p' to pause or 's' to stop.")
+                    last_press_time = current_time
+
+                elif keyboard.is_pressed('s') and current_time - last_press_time > debounce_int:
+                    self._stop_recording()
+                    print("Recording stopped. Transcribing...\n")
+                    break
+
+                time.sleep(0.1)  # Small sleep to prevent high CPU usage
+
             return self._get_predicted_text()
-        except RuntimeError  as e:
-            raise RuntimeError ("Error during audio recording and transcription: ")  from e
+        except RuntimeError as e:
+            raise RuntimeError("Error during audio recording and transcription: ") from e
+            
--- tests/test_waiter.py
+++ tests/test_waiter.py
@@ -1,36 +1,113 @@
-import unittest
-from unittest.mock import MagicMock
-import sys
 import os
+import sys
+import unittest
+from unittest.mock import MagicMock, patch
+import textwrap
+from io import StringIO
+
+# Add the 'app' directory to the system path
 app_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app'))
 sys.path.append(app_dir)
+
+# Import the module under test
 import waiter as wt
 
 
 class TestWaiter(unittest.TestCase):
+    """Test cases for the Waiter class."""
+
     def setUp(self):
+        """Set up the test environment."""
         self.waiter = wt.Waiter()
 
-    # def test_create_prompt(self):
-    #     order = "Sample order for the test"
-    #     expected_prompt = f"""
-    #     Read the restaurant order delimited by triple backticks, step by step analyze what the customer has ordered, and write it down in JSON format with the following keys: dish, quantity, comment.
-    #     ```{order}```
-    #     JSON:
-    #     """.strip()
-    #     prompt = self.waiter.create_old_prompt(order)
-    #     self.assertEqual(prompt, expected_prompt)
+    def test_create_prompt(self):
+        """Test the creation of a prompt."""
+        order = "Sample order for the test"
+        expected_prompt = textwrap.dedent(f"""
+        random message
+        ```{order}```
+        JSON:
+        """).strip()
+        prompt = wt.create_prompt(order, initial_prompt="random message")
+        self.assertEqual(prompt, expected_prompt)
 
     def test_initialize_model(self):
+        """Test the initialization of the model."""
         self.waiter.initialize_model = MagicMock(return_value="Mocked Model")
         model = self.waiter.initialize_model()
         self.assertEqual(model, "Mocked Model")
+        
+    def test_json_to_dict_conversion_and_exception_handling(self):
+        """Test conversion from JSON to dictionary and exception handling."""
+        # Test successful conversion
+        json_str_success = '{"dish": "McChicken", "quantity": 2, "comment": "Extra cheese"}'
+        expected_dict_success = [{"dish": "McChicken", "quantity": 2, "comment": "Extra cheese"}]
+        with self.subTest(case="Successful Conversion"):
+            result_success = wt.json_to_dict(json_str_success)
+            self.assertEqual(result_success, expected_dict_success)
 
-    def test_json_to_dict(self):
-        json_str = '{"dish": "Pizza", "quantity": 2, "comment": "Extra cheese"}'
-        expected_dict = [{"dish": "Pizza", "quantity": 2, "comment": "Extra cheese"}]
-        result = self.waiter.json_to_dict(json_str)
-        self.assertEqual(result, expected_dict)
+        # Test exception handling
+        invalid_json_str = '{"dish": "McChicken", "quantity": 2, "comment": "Extra cheese",}'
+        with self.subTest(case="Exception Handling"):
+            with self.assertRaises(RuntimeError) as context:
+                wt.json_to_dict(invalid_json_str)
+
+    def test_process_order_with_available_items(self):
+        """Test processing an order with available items."""
+        order = [{"dish": "Big Mac", "quantity": 2, "comment": "No pickles"}]
+        self.waiter._process_order(order)
+        ordered_items = self.waiter._ordered
+        self.assertEqual(len(ordered_items), 1)
+        self.assertEqual(ordered_items[0]["dish"], "Big Mac")
+
+    def test_process_order_with_unavailable_items(self):
+        """Test processing an order with unavailable items."""
+        order = [{"dish": "Unknown Dish", "quantity": 1, "comment": "Extra sauce"}]
+        self.waiter._process_order(order)
+        unavailable_items = self.waiter._unavailable
+        self.assertEqual(len(unavailable_items), 1)
+        self.assertEqual(unavailable_items[0], "Unknown Dish")
+
+    def test_create_order(self):
+        """Test the creation of an order."""
+        order_str = 'Give me a Big Mac'
+        self.waiter._llm = MagicMock(
+            return_value='{"dish": "Big Mac", "quantity": 1, "comment": ""}'
+        )
+        self.waiter.create_order(order_str)
+        ordered_items = self.waiter._ordered
+        self.assertEqual(len(ordered_items), 1)
+        self.assertEqual(ordered_items[0]["dish"], "Big Mac")
+
+    def test_print_order(self):
+        # Test when there are no ordered items
+        expected_output = "Sorry, there is nothing in our menu which you ordered\n"
+        self.assertEqual(self.waiter.print_order(), expected_output)
+
+        # Test when there are ordered items
+        self.waiter._ordered = [
+            {"dish": "Burger", "comment": "No onions", "quantity": 2},
+            {"dish": "Pizza", "comment": "", "quantity": 1},
+        ]
+
+        expected_output = "Your order is: 2 Burger (No onions), 1 Pizza\n"
+        self.assertEqual(self.waiter.print_order(), expected_output)
+
+        # Test when there are unavailable items
+        self.waiter._ordered = [{"dish": "Burger", "comment": "No onions", "quantity": 1}]  # Reset ordered items
+        self.waiter._unavailable = ["Soda", "Dessert"]
+
+        expected_output = "Your order is: 1 Burger (No onions)\nUnfortunately we don't have: Soda, Dessert\n"
+        self.assertEqual(self.waiter.print_order(), expected_output)
+
+        # Test when there are both ordered and unavailable items
+        self.waiter._ordered = [
+            {"dish": "Burger", "comment": "", "quantity": 1},
+        ]
+        self.waiter._unavailable = ["Pizza"]
+
+        expected_output = "Your order is: 1 Burger\nUnfortunately we don't have: Pizza\n"
+        self.assertEqual(self.waiter.print_order(), expected_output)
 
 
 if __name__ == '__main__':
