cb73c9ac5087df7319f1791f72080eea5d74c3bb
Gogetanks <01155543@pw.edu.pl>
2023-12-11 22:35:41 +0100

    transcriber base

--- gui/aparro_app/urls.py
+++ gui/aparro_app/urls.py
@@ -7,6 +7,7 @@ from .views import ready_page
 urlpatterns = [
     path('', views.index, name='index'),
     path('ready_page/', ready_page, name='ready_page'),
+    path('transcribe/', views.transcribe_audio, name='transcribe_audio'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- gui/aparro_app/views.py
+++ gui/aparro_app/views.py
@@ -1,7 +1,21 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .transcriber import Transcriber  # Import your Transcriber class
 
+@csrf_exempt
+def transcribe_audio(request):
+    if request.method == 'POST':
+        # Handle the audio file sent from the frontend
+        audio_file = request.FILES.get('audio_data')
 
-# Create your views here.
+        # Use your Transcriber class to transcribe the audio
+        transcriber = Transcriber(model_name="medium.en", device_type="cuda")
+        transcription = transcriber._transcribe_audio(audio_file)
+        
+
+        return JsonResponse({'transcription': transcription})
+    return render(request, 'transcribe.html')
 
 def index(request):
     return render(request, 'index.html')
